0908c1bba0fce1704eebea154d0eab9e12951b1c
chaeyeonie <48678352+chaeyeonie@users.noreply.github.com>
2019-05-26 16:11:34 +0900

    Update make.py

--- make.py
+++ make.py
@@ -1,6 +1,6 @@
 import requests, xmltodict, json
 
-raw_data = requests.get(f'http://apis.data.go.kr/1400119/service/rest/KfniService/btncInfo?serviceKey=KTzkIv%2F6NJ%2BAVkAwqvrQAJntQ1lNEF4YPkO44kO3fYYWVvGDOrDupZBs5d4DCFIIeSs%2BxTiOHJHhs0n3upxFAA%3D%3D&q1=ZDAN000301').content
+raw_data = requests.get(f'http://apis.data.go.kr/1400119/service/rest/KfniService/btncInfo?
 
 xmlObject = xmltodict.parse(raw_data)
 
